e78580403763de89c2043305d205bad77fabbd81
Dave.Potter <dnpotter1@gmail.com>
2025-07-14 10:08:31 +0100

    add did resolver

---
+++ src/opensig-did-resolver.ts
@@ -0,0 +1,37 @@
+import { convertOpenSigId } from './opensig-id';
+
+/**
+ * Returns a DID resolver for the `did:os` method, compatible with the `did-resolver` library.
+ */
+export function getOpenSigDidResolver() {
+  return {
+    os: async (did: string) => {
+      try {
+        const caip10 = convertOpenSigId(did, 'pkh:eip155').slice(8);
+
+        return {
+          '@context': 'https://w3id.org/did-resolution/v1',
+          didResolutionMetadata: { contentType: 'application/did+json' },
+          didDocumentMetadata: {},
+          didDocument: {
+            '@context': 'https://www.w3.org/ns/did/v1',
+            id: did,
+            verificationMethod: [{
+              id: `${did}#controller`,
+              type: 'EcdsaSecp256k1RecoveryMethod2020',
+              controller: did,
+              blockchainAccountId: caip10
+            }],
+            authentication: [`${did}#controller`]
+          }
+        };
+      } catch (err) {
+        return {
+          didResolutionMetadata: { error: 'invalidDid', message: (err as Error).message },
+          didDocument: null,
+          didDocumentMetadata: {}
+        };
+      }
+    }
+  };
+}
